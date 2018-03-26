569a142eed65e02281bd6d6ac0b82ad3a55f7d84
feng1996 <37651077+feng1996@users.noreply.github.com>
2018-03-26 10:50:06 +0800

    Add files via upload

---
+++ PubBizImpl.java
@@ -0,0 +1,33 @@
+package com.feng.biz.impl;
+
+import java.util.LinkedHashMap;
+import java.util.Vector;
+
+import com.feng.biz.PubBiz;
+import com.feng.dao.PubDao;
+import com.feng.dao.impl.PubDaoImpl;
+import com.feng.entity.PubManage;
+
+public class PubBizImpl implements PubBiz {
+
+	
+
+	private PubDao pubDao = new PubDaoImpl();
+	
+	@Override
+	public int insert(String pubTitle, String pubType, String pubUser, String pubTime , String pubContent) {
+		PubManage pub = new PubManage(pubTitle,pubType,pubUser,pubTime,pubContent);
+		return pubDao.insert(pub);
+	}
+	
+	@Override
+	public boolean delPubByTitle(String pubTitle) {
+		pubDao.delPubByTitle(pubTitle);
+		return false;
+	}
+	
+	@Override
+	public PubManage getPubByTitle(String pubTitle) {
+		return pubDao.getPubByTitle(pubTitle);
+	}
+}
